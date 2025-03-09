588d5b2caa3b2499c69ceacbc36f6ca25be17814
Lutin <tclutin01@gmail.com>
2025-03-09 17:16:36 +0500

    add: session table

--- auth-service/migrations/20250305224245_init.sql
+++ auth-service/migrations/20250305224245_init.sql
@@ -4,10 +4,20 @@ SELECT 'up SQL query';
 -- +goose StatementEnd
 CREATE TABLE IF NOT EXISTS public.users (
     user_id BIGSERIAL PRIMARY KEY,
-    username VARCHAR(100) NOT NULL UNIQUE,
+    username VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
+    is_active BOOLEAN NOT NULL DEFAULT FALSE,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS public.sessions (
+    session_id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    refresh_token UUID UNIQUE NOT NULL,
+    expires_at TIMESTAMP NOT NULLz,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES public.users (user_id) ON DELETE CASCADE
 );
 -- +goose Down
 -- +goose StatementBegin
