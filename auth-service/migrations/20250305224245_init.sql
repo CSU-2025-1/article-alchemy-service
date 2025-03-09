e526b7b1219d02d70db29012c3b04d6f64e60a88
Lutin <tclutin01@gmail.com>
2025-03-10 00:55:17 +0500

    ref: migrations

--- auth-service/migrations/20250305224245_init.sql
+++ auth-service/migrations/20250305224245_init.sql
@@ -7,15 +7,15 @@ CREATE TABLE IF NOT EXISTS public.users (
     username VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     is_active BOOLEAN NOT NULL DEFAULT FALSE,
-    password VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS public.sessions (
     session_id BIGSERIAL PRIMARY KEY,
     user_id BIGINT NOT NULL,
-    refresh_token UUID UNIQUE NOT NULL,
-    expires_at TIMESTAMP NOT NULLz,
+    refresh_token VARCHAR(255) UNIQUE NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES public.users (user_id) ON DELETE CASCADE
 );
@@ -23,4 +23,5 @@ CREATE TABLE IF NOT EXISTS public.sessions (
 -- +goose StatementBegin
 SELECT 'down SQL query';
 -- +goose StatementEnd
+DROP TABLE IF EXISTS public.sessions;
 DROP TABLE IF EXISTS public.users;
