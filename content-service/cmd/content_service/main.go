fad3e7771b90877ab147027ec1359a88527c2120
Lutin <tclutin01@gmail.com>
2025-04-01 02:38:02 +0500

    feat: base layer

---
+++ content-service/cmd/content_service/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/CSU-2025-1/article-alchemy-service/content_service/internal/app"
+
+func main() {
+	app.New().Run()
+}
---
+++ content-service/internal/app/app.go
@@ -0,0 +1,78 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"github.com/CSU-2025-1/article-alchemy-service/content_service/internal/config"
+	"github.com/CSU-2025-1/article-alchemy-service/content_service/pkg/client/postgresql"
+	"github.com/CSU-2025-1/article-alchemy-service/content_service/pkg/client/redis"
+	"github.com/CSU-2025-1/article-alchemy-service/content_service/pkg/jwt/manager"
+	"github.com/CSU-2025-1/article-alchemy-service/content_service/pkg/migrator"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type App struct {
+	pool       *pgxpool.Pool
+	grpcServer *grpc.Server
+	cfg        *config.Config
+}
+
+func New() *App {
+	cfg := config.MustLoad()
+
+	dsn := fmt.Sprintf(
+		"postgresql://%v:%v@%v:%v/%v",
+		cfg.Postgres.User,
+		cfg.Postgres.Password,
+		cfg.Postgres.Host,
+		cfg.Postgres.Port,
+		cfg.Postgres.Database)
+
+	postgresClient := postgresql.NewClient(context.Background(), dsn)
+
+	_ = redis.NewClient(cfg.Redis.Host, cfg.Redis.Port)
+
+	migrator.Migrate(postgresClient)
+
+	_ = manager.MustLoadTokenManager(cfg.JWT.Secret)
+
+	grpcServer := grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
+
+	return &App{
+		pool:       postgresClient,
+		grpcServer: grpcServer,
+		cfg:        cfg,
+	}
+}
+
+func (a *App) Run() {
+	listener, err := net.Listen("tcp", net.JoinHostPort(a.cfg.GRPCServer.Host, a.cfg.GRPCServer.Port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	go func() {
+		if err = a.grpcServer.Serve(listener); err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	<-stop
+	a.Stop()
+}
+
+func (a *App) Stop() {
+	a.grpcServer.GracefulStop()
+	a.pool.Close()
+}
