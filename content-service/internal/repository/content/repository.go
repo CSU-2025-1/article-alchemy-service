e4e6b696ccc4b82a6a3fb3b6c33f27a82282862c
Lutin <tclutin01@gmail.com>
2025-04-02 19:32:03 +0500

    feat: added GetById repo layer

--- content-service/internal/repository/content/repository.go
+++ content-service/internal/repository/content/repository.go
@@ -2,7 +2,7 @@ package content
 
 import (
 	"context"
-	"github.com/CSU-2025-1/article-alchemy-service/content_service/internal/domain/entity"
+	"github.com/CSU-2025-1/article-alchemy-service/content_service/internal/domain/model"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -15,7 +15,7 @@ func NewContentRepository(pool *pgxpool.Pool) *ContentRepository {
 	return &ContentRepository{pool}
 }
 
-func (c *ContentRepository) Create(ctx context.Context, model entity.Content) (uint64, error) {
+func (c *ContentRepository) Create(ctx context.Context, model model.Content) (uint64, error) {
 	sql := `INSERT INTO public.contents (user_id, status, data, error, created_at) VALUES ($1, $2, $3, $4, $5) RETURNING contents.content_id`
 
 	row := c.pool.QueryRow(
@@ -35,7 +35,7 @@ func (c *ContentRepository) Create(ctx context.Context, model entity.Content) (u
 	return contentId, nil
 }
 
-func (c *ContentRepository) Update(ctx context.Context, model entity.Content) error {
+func (c *ContentRepository) Update(ctx context.Context, model model.Content) error {
 	sql := `UPDATE public.contents SET user_id = $1, status = $2, data = $3, error = $4 WHERE content_id = $5 RETURNING content_id`
 
 	_, err := c.pool.Exec(ctx, sql, model.UserID, model.Status, model.Data, model.Error, model.ContentID)
@@ -43,12 +43,12 @@ func (c *ContentRepository) Update(ctx context.Context, model entity.Content) er
 	return err
 }
 
-func (c *ContentRepository) GetById(ctx context.Context, contentId uint64) (entity.Content, error) {
+func (c *ContentRepository) GetById(ctx context.Context, contentId uint64) (model.Content, error) {
 	sql := `SELECT * FROM public.contents WHERE content_id = $1`
 
 	row := c.pool.QueryRow(ctx, sql, contentId)
 
-	var content entity.Content
+	var content model.Content
 	err := row.Scan(
 		&content.ContentID,
 		&content.UserID,
@@ -60,7 +60,7 @@ func (c *ContentRepository) GetById(ctx context.Context, contentId uint64) (enti
 	return content, err
 }
 
-func (c *ContentRepository) GetContentsByUserId(ctx context.Context, userId uint64) ([]entity.Content, error) {
+func (c *ContentRepository) GetContentsByUserId(ctx context.Context, userId uint64) ([]model.Content, error) {
 	sql := `SELECT * FROM public.contents WHERE user_id = $1`
 
 	rows, err := c.pool.Query(ctx, sql, userId)
@@ -68,7 +68,7 @@ func (c *ContentRepository) GetContentsByUserId(ctx context.Context, userId uint
 		return nil, err
 	}
 
-	content, err := pgx.CollectRows(rows, pgx.RowToStructByName[entity.Content])
+	content, err := pgx.CollectRows(rows, pgx.RowToStructByName[model.Content])
 	if err != nil {
 		return nil, err
 	}
--- content-service/internal/repository/repository.go
+++ content-service/internal/repository/repository.go
@@ -2,11 +2,12 @@ package repository
 
 import (
 	"context"
-	"github.com/CSU-2025-1/article-alchemy-service/content_service/internal/domain/entity"
+	"github.com/CSU-2025-1/article-alchemy-service/content_service/internal/domain/model"
 )
 
 type ContentRepository interface {
-	Create(ctx context.Context, entity entity.Content) (uint64, error)
-	Update(ctx context.Context, entity entity.Content) (uint64, error)
-	GetContentsByUserId(ctx context.Context, userId uint64) ([]entity.Content, error)
+	Create(ctx context.Context, entity model.Content) (uint64, error)
+	Update(ctx context.Context, entity model.Content) error
+	GetById(ctx context.Context, contentId uint64) (model.Content, error)
+	GetContentsByUserId(ctx context.Context, userId uint64) ([]model.Content, error)
 }
