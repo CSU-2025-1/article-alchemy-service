3085d79bb86a5b1bd5816bb29dd2f42605998a87
Lutin <tclutin01@gmail.com>
2025-04-03 15:10:21 +0500

    fix: CORS

--- api-gateway/main.go
+++ api-gateway/main.go
@@ -37,8 +37,14 @@ func AttachIpAddressMetadata(ctx context.Context, r *http.Request) metadata.MD {
 func AllowCORSMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
 		h.ServeHTTP(w, r)
 	})
 }
